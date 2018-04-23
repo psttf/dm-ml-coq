70b236a75e41a5293adc47fc85456c19e9150d64
ando0116 <anastasiia.domracheva@NetCracker.com>
2018-04-23 20:00:41 +0300

    monoton + fix T0

--- mainv2.v
+++ mainv2.v
@@ -30,8 +30,7 @@ end.
 Definition IsPreservesTrueFunc (n: nat) (f : (BoolFunN n)): Prop :=
   match f with
     | [] => False (* Strange case*)
-    | true :: t => True
-    | false :: t => False
+    | h::t => if List.last (to_list t) false then True else False
 end.
 
 Import Lists.List.
@@ -52,6 +51,7 @@ end.
 
 Definition getComparableSet (n: nat) : list (nat * nat) :=
   match n with
+  | 1 => (cons (0 , 1) nil)
   | 2 => (cons (0 , 1) (cons (1 , 2)  (cons (2 , 3) nil) ))
   | 3 => (cons (0 , 1) (cons (0 , 2)  (cons (0 , 4) ( cons (1 ,3) ( cons (1,5) 
 ( cons (2, 3) ( cons (2, 6) ( cons (3, 7)( cons (4 ,5)( cons (4, 6) 
@@ -59,7 +59,25 @@ Definition getComparableSet (n: nat) : list (nat * nat) :=
   | _ => nil
   end.
 
-Definition isMonotoniusFunc (n: nat) (f : (BoolFunN n)) : Prop :=
-  
+Check fst.
 
+Definition leBool (a b : bool) : bool :=
+  match a, b with
+  | true, false => false
+  | _, _ => true
+  end.
+
+
+Fixpoint isMonotoniusFuncHelp {n: nat} (f : (BoolFunN n)) ( comparation: list (nat * nat)) : Prop :=
+  let fl := to_list f in match comparation with
+  | List.nil => True
+  | h::t => if  leBool (nth (fst h) fl false) (nth (snd h) fl false)  then isMonotoniusFuncHelp f t  else False
+  end.
+
+Definition isMonotoniusFunc (n: nat) (f : (BoolFunN n)) : Prop := isMonotoniusFuncHelp f (getComparableSet n).
+  
+(* Определит линейность, ввести понятие системы функций, 
+написать проверки на на не принадлежность к классам для системы функций,
+тогда теорема о полноте: T0 -> T1 -> S -> M -> L -> Full.
+А проверка конкретной системы будет ??????*)
 
