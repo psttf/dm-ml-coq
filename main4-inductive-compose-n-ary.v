f55bca90c55f5278620e969ec6cee1b6b72f8525
ando0116 <anastasiia.domracheva@NetCracker.com>
2018-05-15 17:50:15 +0300

    preserves true for n-ary

--- main4-inductive-compose-n-ary.v
+++ main4-inductive-compose-n-ary.v
@@ -5,29 +5,11 @@ Definition BoolFun := Prop -> Prop.
 Inductive Bool: Prop :=
   True: Bool | False: Bool.
 
-Print Bool_ind.
-
-Definition id (x: Bool) := x.
-Definition ConstTrue  (_ : Bool) := True.
-Definition ConstFalse (_ : Bool) := False.
-Definition Not (x : Bool) : Bool := match x with
-  | True => False
-  | False => True
-end.
-
-Print t.
-
-Print nat.
-
 Fixpoint t_n (A: Type) (n: nat) (x: A): t A n := 
   match n with
     | O => nil A
     | S m => cons A x m (t_n A m x)
   end.
-
-Check t_n Bool 3 False.
-
-Eval compute in t_n Bool 3 False.
 
 Inductive preserves_false (n: nat) (f: t Bool n -> Bool) := 
   preserves: (f(t_n Bool n False) = False) -> preserves_false n f.
@@ -65,13 +47,6 @@ intros.
 apply comp_rewrite.
 Qed.
 
-Print t.
-
-Check t Type 0.
-
-Check Forall.
-
-Check Vector.nth.
 
 Lemma preserves_false_vector: forall {n: nat} {m: nat} (gs: t (t Bool n -> Bool) m),
   Forall (fun (g: t Bool n -> Bool) => preserves_false n g) gs -> 
@@ -109,18 +84,34 @@ exact e.
 exact H0.
 Qed.
 
-Inductive preserves_true (x: Bool -> Bool) := 
-  preserve_true: (x(True) = True) -> preserves_true x.
+Inductive preserves_true (n: nat) (f: t Bool n -> Bool) := 
+  preserve_true: (f(t_n Bool n True) = True) -> preserves_true n f.
+
+Lemma preserves_true_vector: forall {n: nat} {m: nat} (gs: t (t Bool n -> Bool) m),
+  Forall (fun (g: t Bool n -> Bool) => preserves_true n g) gs -> 
+    (map ( fun (g: t Bool n -> Bool) => g (t_n Bool n True) ) gs) = (t_n Bool m True).
+Proof.
+intros.
+induction H.
+auto.
+simpl.
+induction H.
+rewrite e.
+induction map.
+auto.
+rewrite IHForall.
+auto.
+Qed.
+
 
 Definition preserves_true_is_composed_closed: compose_closed preserves_true.
 Proof.
 apply compose_is_c.
 intros.
 induction H.
-induction H0.
 apply preserve_true.
 unfold compose.
-rewrite e.
-rewrite e0.
-exact eq_refl.
+rewrite preserves_true_vector.
+exact e.
+exact H0.
 Qed.
